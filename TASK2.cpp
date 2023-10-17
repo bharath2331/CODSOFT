cbdcf0a68d27d2d57cdb38b0ac4aa4de37c1e4d9
bharath2331 <141639455+bharath2331@users.noreply.github.com>
2023-10-17 12:54:02 +0530

    TASK2.cpp

--- TASK2.cpp
+++ TASK2.cpp
@@ -3,43 +3,31 @@
 using namespace std;
 
 int main() {
-    double num1, num2;
     char operation;
+    double num1, num2;
 
-    cout << "Enter first number: ";
-    cin >> num1;
-
-    cout << "Enter second number: ";
-    cin >> num2;
-
-    cout << "Choose an operation (+, -, *, /): ";
+    cout << "Simple Calculator" << endl;
+    cout << "Enter operator (+, -, *, /): ";
     cin >> operation;
 
-    double result;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
 
     switch (operation) {
         case '+':
-            result = num1 + num2;
-            cout << "Result: " << result << endl;
+            cout << "Result: " << num1 + num2 << endl;
             break;
         case '-':
-            result = num1 - num2;
-            cout << "Result: " << result << endl;
+            cout << "Result: " << num1 - num2 << endl;
             break;
         case '*':
-            result = num1 * num2;
-            cout << "Result: " << result << endl;
+            cout << "Result: " << num1 * num2 << endl;
             break;
         case '/':
-            if (num2 != 0) {
-                result = num1 / num2;
-                cout << "Result: " << result << endl;
-            } else {
-                cout << "Error: Division by zero is not allowed." << endl;
-            }
+            cout << "Result: " << num1 / num2 << endl;
             break;
         default:
-            cout << "Invalid operation. Please choose from +, -, *, /" << endl;
+            cout << "Error: Invalid operator" << endl;
             break;
     }
 
